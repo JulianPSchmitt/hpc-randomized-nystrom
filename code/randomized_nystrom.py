184717383350b04fdb170e11b80ca2b79f396bf5
Julian Schmitt <julian.p.schmitt@gmail.com>
2023-11-19 17:28:00 +0100

    Add sequential Nyström (cholesky) and mnist data

---
+++ code/randomized_nystrom.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy import linalg
+import scipy
+from hadamard_transform import hadamard_transform
+import torch
+
+
+def rand_nystrom_cholesky(A, Omega, rank):
+    """Compute the randomized Nyström rank k approximation given the sketching
+    matrix Omega (uses Cholesky decomposition)"""
+    C = A@Omega
+    B = Omega.T@C
+    L = np.linalg.cholesky(B)
+    Z = linalg.solve_triangular(L, C.T).T
+    Q, R = np.linalg.qr(Z)
+    U_t, Sigma, V_t = scipy.sparse.linalg.svds(R, k=rank)
+    U = Q@U_t
+    return U, np.diag(Sigma**2)
+
+
+def generate_SRHT(l, m):
+    """Generate a subsampled randomized Hadamard transform (SRHT) skteching
+    matrix"""
+    d = np.array([1 if np.random.random() < 0.5 else -1 for i in range(m)])
+    omega = np.diag(np.sqrt(m/l)*d)
+    omega = hadamard_transform(torch.from_numpy(omega)).numpy()
+    return omega[np.random.choice(range(m), l, replace=False), :]
