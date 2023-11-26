10b33cada6cd54c977837e15cb96151c9d0f1b95
Julian Schmitt <97178428+JulianPSchmitt@users.noreply.github.com>
2023-11-26 19:34:18 +0100

    Nyström Approximation via Cholesky applied to MNIST (#1)
    * Add sequential Nyström (based on cholesky) applied to mnist data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hpc-randomized-nystrom
+# Randomized Nyström Low-Rank Approximation
 We aim to study the randomized Nyström algorithm for computing a low-rank
 approximation of a large and dense matrix $A \in \mathbb{R}^{n \times n}$ that
 is symmetric positive semidefinite. Given a sketching matrix $\Omega \in
---
+++ code/randomized_nystrom.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import linalg
+import scipy
+np.random.seed(2002)
+
+
+def rand_nystrom_cholesky(A, Omega, rank):
+    """Compute the randomized Nyström rank k approximation given the sketching
+    matrix Omega (uses Cholesky decomposition)"""
+    C = A@Omega
+    B = Omega.T@C
+    L = np.linalg.cholesky(B)
+    Z = linalg.solve_triangular(L, C.T, lower=True).T
+    Q, R = np.linalg.qr(Z)
+    U_t, Sigma, V_t = scipy.sparse.linalg.svds(R, k=rank)
+    U = Q@U_t
+    return U, np.diag(Sigma**2)
+
+
+def generate_SRHT(l, m):
+    """Generate a subsampled randomized Hadamard transform (SRHT) skteching
+    matrix"""
+    d = np.array([1 if np.random.random() < 0.5 else -1 for i in range(m)])
+    omega = np.diag(np.sqrt(m/l)*d)
+    omega = (linalg.hadamard(m)/np.sqrt(m)) @ omega
+    return omega[np.random.choice(range(m), l, replace=False), :].T
