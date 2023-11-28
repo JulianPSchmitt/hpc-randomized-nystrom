abec1e0b23974ff1fea8f48e3a1a756c807bd835
Julian Schmitt <97178428+JulianPSchmitt@users.noreply.github.com>
2023-11-28 10:45:16 +0100

    Add parallel Nyström Approximation (#4)
    * Add TSQR
    * Add parallel sketching
    * Add parallel Nystrom approximation
    ---------
    
    Co-authored-by: TM PC WSL <christian.mikkelstrup@gmail.com>

--- code/randomized_nystrom.py
+++ code/randomized_nystrom.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy import linalg
 import scipy
+from mpi4py import MPI
+import sketching
+import tsqr
 np.random.seed(2002)
 
 
@@ -17,10 +20,22 @@ def rand_nystrom_cholesky(A, Omega, rank):
     return U, np.diag(Sigma**2)
 
 
-def generate_SRHT(l, m):
-    """Generate a subsampled randomized Hadamard transform (SRHT) skteching
-    matrix"""
-    d = np.array([1 if np.random.random() < 0.5 else -1 for i in range(m)])
-    omega = np.diag(np.sqrt(m/l)*d)
-    omega = (linalg.hadamard(m)/np.sqrt(m)) @ omega
-    return omega[np.random.choice(range(m), l, replace=False), :].T
+def rand_nystrom_cholesky_parallel(A, n: int, l: int, truncate_rank: int, comm: MPI.Comm, seed=2002):
+    """Compute the randomized Nyström rank k approximation given the sketching
+    matrix Omega (uses Cholesky decomposition)"""
+    rank = comm.Get_rank()
+
+    B, C = sketching.sketch_2D_BSRHT(A, n, l, comm, seed)
+
+    Z = None
+    if rank == 0:
+        L = np.linalg.cholesky(B)
+        Z = linalg.solve_triangular(L, C.T, lower=True).T
+    Q, R = tsqr.tsqr(Z, n, l, comm, True)
+
+    U = S = None
+    if rank == 0:
+        U_t, Sigma, V_t = scipy.sparse.linalg.svds(R, k=truncate_rank)
+        U = Q@U_t
+        S = np.diag(Sigma**2)
+    return U, S
