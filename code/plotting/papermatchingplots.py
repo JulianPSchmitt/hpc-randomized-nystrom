7c38372b1f60fd631d83e267b8f4e7b5600ef80d
TM PC WSL <christian.mikkelstrup@gmail.com>
2023-12-04 14:40:56 +0100

    Use existing build_A. Try statem. for paperplots

--- code/plotting/papermatchingplots.py
+++ code/plotting/papermatchingplots.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 from randomized_nystrom import rand_nystrom_cholesky
-from plotting import computeA
+from mnist import build_A
 from os.path import join
 
 
 def errorplot(X: np.ndarray, n: int, c: float, ls: list[int], ks: list[int]):
     X = X[:n, :]
     print(f"Shape: {X.shape}")
-    A = computeA(X, cval=c)
+    A = build_A(X, c=c, save=False)
 
     colors = "krbgy"
 
@@ -54,36 +54,61 @@ def paperMatchingPlots(
     n: int = 2**10,
     plotTitleSpecifier: str = "",
 ):
-    X = np.load(MNIST_X_path)
-    ls = [600, 1000, 2000]
-    ks = [200, 300, 400, 500, 600]
+    try:
+        X = np.load(MNIST_X_path)
+        ls = [600, 1000, 2000]
+        ks = [200, 300, 400, 500, 600]
 
-    # MNIST plot
-    errorplot(X=X, n=n, c=100**2, ls=ls, ks=ks)
-    plt.savefig(
-        join(
-            savepath,
-            "MNIST_trace_relative_error" + plotTitleSpecifier + ".png",
+        # MNIST plot
+        errorplot(X=X, n=n, c=100**2, ls=ls, ks=ks)
+        plt.savefig(
+            join(
+                savepath,
+                "MNIST_trace_relative_error" + plotTitleSpecifier + ".png",
+            )
         )
-    )
-
-    X2 = np.load(YearMSD_X_path)
-    ls = [600, 1000, 2000, 2500, 3000]
-    ks = [100, 200, 500, 1000]
-    errorplot(X=X2, n=n, c=(1e4) ** 2, ls=ls, ks=ks)
-    plt.savefig(
-        join(
-            savepath,
-            "YearMSD_10_4_trace_relative_error" + plotTitleSpecifier + ".png",
+        plt.show()
+    except Exception as err:
+        print(
+            f"Was unable to create the MNIST error plot does to error: {err}"
         )
-    )
 
-    errorplot(X=X2, n=n, c=(1e5) ** 2, ls=ls, ks=ks)
-    plt.savefig(
-        join(
-            savepath,
-            "YearMSD_10_5_trace_relative_error" + plotTitleSpecifier + ".png",
+    try:
+        X2 = np.load(YearMSD_X_path)
+        ls = [600, 1000, 2000, 2500, 3000]
+        ks = [100, 200, 500, 1000]
+        errorplot(X=X2, n=n, c=(1e4) ** 2, ls=ls, ks=ks)
+        plt.savefig(
+            join(
+                savepath,
+                "YearMSD_10_4_trace_relative_error"
+                + plotTitleSpecifier
+                + ".png",
+            )
         )
-    )
+        plt.show()
+    except Exception as err:
+        print(
+            "Was unable to create the YearMSD 1e-4 error plot does to error:"
+            f" {err}"
+        )
 
-    plt.show()
+    try:
+        X2 = np.load(YearMSD_X_path)
+        ls = [600, 1000, 2000, 2500, 3000]
+        ks = [100, 200, 500, 1000]
+        errorplot(X=X2, n=n, c=(1e5) ** 2, ls=ls, ks=ks)
+        plt.savefig(
+            join(
+                savepath,
+                "YearMSD_10_5_trace_relative_error"
+                + plotTitleSpecifier
+                + ".png",
+            )
+        )
+        plt.show()
+    except Exception as err:
+        print(
+            "Was unable to create the YearMSD 1e-5 error plot does to error:"
+            f" {err}"
+        )
