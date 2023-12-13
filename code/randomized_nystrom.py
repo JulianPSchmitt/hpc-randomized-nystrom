8c9fc6e905c9df444ca7f664c9ae77076deec730
Julian Schmitt <97178428+JulianPSchmitt@users.noreply.github.com>
2023-12-13 21:39:45 +0100

    Add Nyström via SVD (#10)

--- code/randomized_nystrom.py
+++ code/randomized_nystrom.py
@@ -8,10 +8,32 @@ import tsqr
 np.random.seed(2002)
 
 
+def rand_nystrom_svd(A, Omega, rank):
+    """
+    Compute the randomized Nyström rank k approximation given the sketching
+    matrix Omega (uses SVD instead of Cholesky). The method relies on the
+    Nyström approximation which incorporates the 'Q' factor of QR decomposition.
+    """
+    C = A @ Omega
+    B = Omega.T @ C
+    Ub, Sb = linalg.svd(B)[0:2]
+    L = Ub@np.diag(np.sqrt(Sb))
+    Z = np.linalg.solve(L, C.T).T
+    Q, R = np.linalg.qr(Z)
+    if min(R.shape) == rank:
+        U_t, Sigma, V_t = linalg.svd(R)
+    else:
+        U_t, Sigma, V_t = scipy.sparse.linalg.svds(R, k=rank)
+    U = Q @ U_t
+    return U, np.diag(Sigma**2)
+
+
 def rand_nystrom_cholesky(A, Omega, rank):
-    """Compute the randomized Nyström rank k approximation given the sketching
+    """
+    Compute the randomized Nyström rank k approximation given the sketching
     matrix Omega (uses Cholesky decomposition). The method relies on the Nyström
-    approximation which incorporates the 'Q' factor of QR decomposition."""
+    approximation which incorporates the 'Q' factor of QR decomposition.
+    """
     C = A @ Omega
     B = Omega.T @ C
     L = np.linalg.cholesky(B)
@@ -26,10 +48,12 @@ def rand_nystrom_cholesky(A, Omega, rank):
 
 
 def rand_nystrom_cholesky_no_Q(A, Omega, rank):
-    """Compute the randomized Nyström rank k approximation given the sketching
+    """
+    Compute the randomized Nyström rank k approximation given the sketching
     matrix Omega (uses Cholesky decomposition). The method does not use the 'Q'
     factor of the QR decomposition to increase efficency (potentially at the
-    cost of less stability)."""
+    cost of less stability).
+    """
     C = A @ Omega
     B = Omega.T @ C
     L = np.linalg.cholesky(B)
@@ -41,6 +65,67 @@ def rand_nystrom_cholesky_no_Q(A, Omega, rank):
         U_t, Sigma, V_t = scipy.sparse.linalg.svds(R, k=rank)
     U = Z @ V_t.T @ np.diag(Sigma**(-1))
     return U, np.diag(Sigma**2)
+
+
+def rand_nystrom_svd_parallel(
+    A, n: int, l: int, truncate_rank: int, comm: MPI.Comm,
+        seed=2002, sketching_mode='BSRHT'
+):
+    """
+    Compute the randomized Nyström rank k approximation of A using SVD instead
+    of Cholesky decomposition.
+
+    A is sketched using a 2D grid of p×p processors:
+    1) Compute C_ij = A_ij Ω_j and sum reduce row-wise
+    2) Compute B_i = Ω_i C_i using the block-row distribution of C and sum
+       reduce
+
+    C is kept row-wise distributed after skteching, to reuse the partitioning
+    when applying TSQR. For this reason, the Z = C*L^(-1) is computed in
+    parallel.
+
+    The method relies on the Nyström approximation which incorporates the 'Q'
+    factor of QR decomposition. Therefore, the TSQR explicitely computes Q. This
+    increases stability but reduces efficency.
+
+    Finally, the matrices U and S are returned exclusively on the root
+    processor.
+    """
+    rank = comm.Get_rank()
+    # Sketch A in parallel using a 2D processor grid
+    B, C = sketching.sketch_2D(A, n, l, comm, seed, sketching_mode)
+
+    # Solve a triangular system using row-block distribution of C
+    L = np.zeros((l, l))
+    if rank == 0:
+        Ub, Sb = linalg.svd(B)[0:2]
+        L = Ub@np.diag(np.sqrt(Sb))
+    comm.Bcast(L, root=0)
+    Z = np.linalg.solve(L, C.T).T
+
+    # Execute TSQR
+    Q, R = tsqr.tsqr(Z, n, comm, True)
+
+    # Compute SVD sequential
+    S = None
+    U_t = np.zeros((truncate_rank, l), dtype=float)
+    if rank == 0:
+        if min(R.shape) == truncate_rank:
+            U_t, Sigma, V_t = linalg.svd(R)
+        else:
+            U_t, Sigma, V_t = scipy.sparse.linalg.svds(R, k=truncate_rank)
+        U_t = np.array(U_t)
+        S = np.diag(Sigma**2)
+
+    # Exploit the row-block distribution of Q to compute matrix-matrix product
+    # in parallel
+    comm.Bcast(U_t, root=0)
+    if rank != 0:
+        U_t = U_t.T
+    U_local = Q @ U_t
+    U = np.empty((n, truncate_rank))
+    comm.Gatherv(U_local, U)
+    return U, S
 
 
 def rand_nystrom_cholesky_parallel(
